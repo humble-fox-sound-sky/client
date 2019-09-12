823c33626b74d173c484af93d980c654daf97497
User <SultanRayhan32>
2019-09-12 21:15:55 +0700

    jalan

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="w-full h-screen" id="main-bg">
+    <button2 @apa="fetchData()"></button2>
+    <Header></Header>
+    <Content :songs="songs"></Content>
+    <footer class="border-2 border-transparent rounded-full">
+      <div class="text-xs text-right mr-2">
+        <i class="far fa-copyright"></i> 2019, Soundsky
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Header from './Header';
+import Content from './Content'
+import axios from 'axios';
+import button2 from './buttonAdd'
+export default {
+    components : {
+        Header,
+        Content,
+        button2
+    },
+    created (){
+        this.fetchData()
+    },
+    data (){
+      return {
+        songs : []
+      }
+    },
+    methods : {
+        fetchData (){
+            axios.get('http://localhost:3000/post')
+            .then(({data})=>{
+                this.songs = data
+            })
+            .catch(console.log)
+        },
+        masuk (data){
+          this.songs.unshift(data)
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/ContentItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div
+      class="h-24 h-12 border-transparent border-2 shadow flex"
+    >
+      <div class="w-1/2 p-4">
+        <h3>{{song.name}}</h3>
+        <p class="text-sm">Hey check this out!</p>
+      </div>
+      <div class="w-1/2 flex justify-center items-center">
+        <audio controls class="h-6 w-64">
+          <source :src="song.url" />
+        </audio>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props : ["song"]
+};
+</script>
+
+<style>
+</style>
